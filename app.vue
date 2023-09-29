9bd8223931188abae253fe2434bd71f6bbde840b
Yaroslav Ovdii <yaroslav.ovdii@uproad.com>
2023-09-29 12:08:43 +0300

    feat: added editor component

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
 <template>
   <NoteCard :title="''" :content="''" :createdAt="new Date().getTime()" />
+  <Editor v-model:value="editorValue" />
+
+  <span v-html="editorValue"></span>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const editorValue = ref('')
+
+</script>
---
+++ components/Editor.vue
@@ -0,0 +1,23 @@
+<template>
+  <textarea v-bind:value="value" v-on:input="$emit('update:value', $event.target.value)" class="editor"></textarea>
+</template>
+
+<script setup>
+defineProps({
+  value: String
+})
+</script>
+
+<style scoped>
+textarea {
+  width: 100%;
+  height: 100vh;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: none;
+  background-color: #f8f8f8;
+  font-size: 16px;
+  resize: none;
+  outline: none;
+}
+</style>
