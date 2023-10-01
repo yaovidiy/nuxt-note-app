bf1eaf0d8e5c707973e1815f1a2239bbce7a5454
Yaroslav Ovdii <yaroslav.ovdii@uproad.com>
2023-10-01 11:45:08 +0300

    feat: Added mobile layout

--- app.vue
+++ app.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="sidebar">
+      <div :class="{ active: isMobileMenuOpened }" class="sidebar">
         <div class="controls">
-          <button class="btn">
-            <svg fill="gray" width="16" height="16" viewBox="0 0 32 32"
-              style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" version="1.1"
-              xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:serif="http://www.serif.com/"
-              xmlns:xlink="http://www.w3.org/1999/xlink">
-              <path
-                d="M14.5,14.501l-10.502,0c-0.828,0 -1.5,0.673 -1.5,1.5c0,0.828 0.672,1.5 1.5,1.5l10.502,0l-0.001,10.502c0,0.828 0.672,1.5 1.5,1.501c0.828,-0 1.5,-0.673 1.5,-1.5l0.001,-10.503l10.502,0c0.828,0 1.5,-0.672 1.5,-1.5c0,-0.827 -0.672,-1.5 -1.5,-1.5l-10.502,0l0.001,-10.501c-0,-0.828 -0.672,-1.501 -1.5,-1.501c-0.828,0 -1.5,0.672 -1.5,1.5l-0.001,10.502Z" />
-              <g id="Icon" />
-            </svg>
-          </button>
+          <div class="btns">
+            <button class="btn">
+              <svg fill="gray" width="24" height="24" viewBox="0 0 32 32"
+                style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" version="1.1"
+                xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:serif="http://www.serif.com/"
+                xmlns:xlink="http://www.w3.org/1999/xlink">
+                <path
+                  d="M14.5,14.501l-10.502,0c-0.828,0 -1.5,0.673 -1.5,1.5c0,0.828 0.672,1.5 1.5,1.5l10.502,0l-0.001,10.502c0,0.828 0.672,1.5 1.5,1.501c0.828,-0 1.5,-0.673 1.5,-1.5l0.001,-10.503l10.502,0c0.828,0 1.5,-0.672 1.5,-1.5c0,-0.827 -0.672,-1.5 -1.5,-1.5l-10.502,0l0.001,-10.501c-0,-0.828 -0.672,-1.501 -1.5,-1.501c-0.828,0 -1.5,0.672 -1.5,1.5l-0.001,10.502Z" />
+                <g id="Icon" />
+              </svg>
+            </button>
 
-          <button class="btn">
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <button class="btn">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6"
+                  stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </button>
+          </div>
+          <button @click="() => isMobileMenuOpened = false" class="btn mobile">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
-                d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6"
-                stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z"
+                fill="gray" />
             </svg>
           </button>
         </div>
@@ -29,24 +38,33 @@
       </div>
       <div class="content">
         <div class="controls">
-          <button class="btn">
-            <svg width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <g id="Complete">
-                <g id="edit">
-                  <g>
-                    <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke="gray"
-                      stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-                    <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke="gray"
-                      stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+          <div class="btns">
+            <button @click="() => isMobileMenuOpened = true" class="btn mobile">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 18L20 18" stroke="gray" stroke-width="2" stroke-linecap="round" />
+                <path d="M4 12L20 12" stroke="gray" stroke-width="2" stroke-linecap="round" />
+                <path d="M4 6L20 6" stroke="gray" stroke-width="2" stroke-linecap="round" />
+              </svg>
+            </button>
+            <button v-if="activeCard !== null" class="btn">
+              <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <g id="Complete">
+                  <g id="edit">
+                    <g>
+                      <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke="gray"
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+                      <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke="gray"
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+                    </g>
                   </g>
                 </g>
-              </g>
-            </svg>
-          </button>
+              </svg>
+            </button>
+          </div>
 
           <label for="search">
             <button class="btn">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
                   stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -68,12 +86,13 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
 
-let activeCard = ref(null)
-let editMode = false
-let activeEditorValue = ref('')
+const isMobileMenuOpened = ref(false)
+const activeCard = ref(null)
+const editMode = ref(false)
+const activeEditorValue = ref('')
 
 const placeholderCards = [
   {
@@ -104,13 +123,17 @@ const placeholderCards = [
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 16px;
-  height: 16px;
+  width: 24px;
+  height: 24px;
   margin: 0;
   padding: 0;
   background: none;
   border: none;
   cursor: pointer;
+}
+
+.btns {
+  display: flex;
 }
 
 .placeholder {
@@ -134,11 +157,12 @@ label {
   position: relative;
 
   input.search {
-    padding-left: 24px;
+    height: 24px;
+    padding-left: 32px;
     border-radius: 4px;
     border-color: gray;
   }
-  
+
   .btn {
     position: absolute;
     top: 2px;
@@ -152,7 +176,7 @@ label {
 
 .content {
   width: 100%;
-  padding: 0 16px;
+  padding: 0 4px;
 
   .controls {
     display: flex;
@@ -161,19 +185,33 @@ label {
 }
 
 .sidebar {
+  position: absolute;
+  left: -1000px;
   width: 100%;
   border-right: 1px solid #f0f0f0;
-  padding: 0 8px;
+  padding: 0 4px;
   height: 100vh;
   max-height: calc(100vh - 2rem);
   overflow: auto;
+  background-color: #FFFFFF;
+  z-index: 3;
+  transition: all .3s;
+
+  &.active {
+    left: 0px;
+  }
 
   .controls {
     display: flex;
     margin-bottom: 16px;
+    justify-content: space-between;
 
-    .btn:first-child {
-      margin-right: 16px;
+    .btns {
+      display: flex;
+
+      .btn:first-child {
+        margin-right: 16px;
+      }
     }
   }
 }
@@ -185,11 +223,17 @@ label {
 
 @media (min-width: 768px) {
   .sidebar {
+    position: relative;
+    left: 0;
     flex-basis: 25%;
   }
 
   .content {
     flex-basis: 75%;
+  }
+
+  .mobile {
+    display: none;
   }
 }
 </style>
